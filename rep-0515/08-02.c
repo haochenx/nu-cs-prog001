fbaebd8b73fefd67562265c4fc9c2ed00b089f85
Haochen Xie <haochen@xie.name>
2015-05-16 13:03:09 +0900

    rep-0515/ - add 08-{02,03}.c
    Signed-off-by: Haochen Xie <haochen@xie.name>

---
+++ rep-0515/08-02.c
@@ -0,0 +1,23 @@
+/*
+  FILE     : 08-02.c
+  REF      : http://www.ipl.cs.is.nagoya-u.ac.jp/~prog1/control00.htm
+  AUTHOR   : Haochen Xie
+  CREATION : 20150508
+ */
+
+#include <stdio.h>
+
+int main (void) {
+  int a;
+
+  printf ("Input a number: ");
+  scanf ("%d", &a);
+
+  if (a % 2 == 0) {
+    printf ("%d is an even number.\n", a);
+  } else {
+    printf ("%d is an odd number.\n", a);
+  }
+
+  return 0;
+}
---
+++ rep-0515/08-03.c
@@ -0,0 +1,27 @@
+/*
+  FILE     : 08-03.c
+  REF      : http://www.ipl.cs.is.nagoya-u.ac.jp/~prog1/control00.htm
+  AUTHOR   : Haochen Xie
+  CREATION : 20150508
+ */
+
+#include <stdio.h>
+
+int main (void) {
+  int a;
+
+  printf ("Input a number: ");
+  scanf ("%d", &a);
+
+  // hey! so what about a==0??
+  switch (a < 0) {
+  case 1:
+    printf ("%d is a negative number.\n", a);
+    break;
+  case 0:
+    printf ("%d is a positive number.\n", a);
+    break;
+  }
+
+  return 0;
+}
