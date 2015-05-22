2ed016a9404be5be7b4349b385af1cbfcc5b50fb
Haochen Xie <xie.haochen@f.mbox.nagoya-u.ac.jp>
2015-05-22 11:47:38 +0900

    rep-0522/ - add 11-01.c
    Signed-off-by: Haochen Xie <xie.haochen@f.mbox.nagoya-u.ac.jp>

---
+++ rep-0522/11-01.c
@@ -0,0 +1,17 @@
+/*
+  FILE     : 11-01.c
+  REF      : http://www.ipl.cs.is.nagoya-u.ac.jp/~prog1/priprocessor.htm
+  AUTHOR   : Haochen Xie
+  CREATION : 20150522
+ */
+
+#include <stdio.h>
+#define PrnChar(str) {printf("%s\n",str);};
+
+int main (void) {
+  char *s = "August";
+  PrnChar (s);
+
+  return 0;
+}
+
