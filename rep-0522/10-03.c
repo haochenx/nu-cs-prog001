4704565779990ec3c4e0e006057cc4ed67c72c52
Haochen Xie <xie.haochen@f.mbox.nagoya-u.ac.jp>
2015-05-22 11:37:32 +0900

    rep-0522/ - add 10-03.c
    Signed-off-by: Haochen Xie <xie.haochen@f.mbox.nagoya-u.ac.jp>

---
+++ rep-0522/10-03.c
@@ -0,0 +1,27 @@
+/*
+  FILE     : 10-03.c
+  REF      : http://www.ipl.cs.is.nagoya-u.ac.jp/~prog1/io.htm
+  AUTHOR   : Haochen Xie
+  CREATION : 20150522
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void) {
+  float x1, x2, x3, y;
+  FILE *fin;
+
+  fin = fopen ("init", "r");
+
+  fscanf (fin, "%f", &x1);
+  fscanf (fin, "%f", &x2);
+  fscanf (fin, "%f", &x3);
+
+  y = x1 * x1 + x2 * x2 + x3 * x3;
+
+  printf ("Result = %f\n", y);
+
+  return 0;
+}
+
