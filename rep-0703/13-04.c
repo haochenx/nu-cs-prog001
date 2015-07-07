2719b3ad7f302d9e2341d9aeec26b32eb1714489
Haochen Xie <xie.haochen@f.mbox.nagoya-u.ac.jp>
2015-07-07 13:53:57 +0900

    rep-0703/ - add 13-04.c
    Signed-off-by: Haochen Xie <xie.haochen@f.mbox.nagoya-u.ac.jp>

---
+++ rep-0703/13-04.c
@@ -0,0 +1,34 @@
+/*
+  FILE     : 13-04.c
+  REF      : http://www.ipl.cs.is.nagoya-u.ac.jp/~prog1/function02.htm
+  AUTHOR   : Haochen Xie
+  CREATION : 20150707
+ */
+
+#include <stdio.h>
+
+int is_prime (int);
+
+int main (void) {
+  int x;
+
+  printf ("Input an integer: ");
+  scanf ("%d", &x);
+
+  printf ("%d is (1) or isn't (0) an integer: %d\n",
+	  x, is_prime(x));
+
+  return 0;
+}
+
+int is_prime (int x) {
+  int i;
+
+  for (i = 2; i <= x / 2; ++i) {
+    if (x % i == 0) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
